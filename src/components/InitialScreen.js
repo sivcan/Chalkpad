4e401d09928a34da6f51459322818057174e6564
Sivcan Singh <sivcansingh@me.com>
2017-05-26 00:07:49 +0530

    Fixed the initial screen login button.

--- src/components/InitialScreen.js
+++ src/components/InitialScreen.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Image, TouchableWithoutFeedback, StatusBar, View } from 'react-native';
+import { Image, StatusBar, View, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { CardSection, Button } from './common';
 
 const InitialScreen = () => {
     const { backgroundImage } = styles;
     return (
         <View style={{ flex:1 }}>
             <StatusBar backgroundColor='blue' barStyle='light-content' />
-            <TouchableWithoutFeedback onPress={() => Actions.login()}>
                 <Image
                 source={require('../../images/InitialScreen.png')}
                 style={backgroundImage}
-                />
-            </TouchableWithoutFeedback>
+                >
+                <CardSection style={{ marginTop: 408, backgroundColor: 'transparent', borderBottomWidth: 0 }}>
+                    <Button onPress={() => Actions.login()} style={{  backgroundColor: 'transparent', borderColor: 'transparent', borderRadius: 50, }}>
+
+                    </Button>
+                </CardSection>
+                </Image>
         </View>
     );
 };
@@ -23,7 +28,8 @@ const styles = {
         resizeMode: 'cover',
         justifyContent: 'center',
         alignItems: 'center',
-        width: null
+        width: null,
+        backgroundColor: 'transparent'
     }
 };
 
